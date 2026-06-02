ff9e7ec8fc1cd5b98c09b7b796dca1f88f3ad2dc
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-06-02 13:39:33 +0530

    Frontend fixes

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -5,10 +5,20 @@ import { motion } from "framer-motion";
 function Hero() {
 
   return (
-    <section
-  id="home"
-  className="h-screen flex flex-col md:flex-row items-center justify-center px-10"
->
+    <section id="home"
+      className="
+      min-h-screen
+      flex
+      flex-col-reverse
+      md:flex-row
+      items-center
+      justify-center
+      px-6
+      md:px-16
+      py-20
+      gap-10
+    "
+    >
 
       <div className="flex-1">
 
@@ -16,7 +26,15 @@ function Hero() {
           initial={{opacity:0,y:50}}
           animate={{opacity:1,y:0}}
           transition={{duration:1}}
-          className="text-6xl font-bold leading-tight"
+          className="
+                    text-4xl
+                    sm:text-5xl
+                    md:text-6xl
+                    font-bold
+                    leading-tight
+                    text-center
+                    md:text-left
+                    "
         >
           Hi, I'm <br />
 
@@ -26,12 +44,31 @@ function Hero() {
 
         </motion.h1>
 
-        <p className="mt-6 text-xl text-gray-300 max-w-xl">
+        <p className="
+                    mt-6
+                    text-base
+                    sm:text-lg
+                    md:text-xl
+                    text-gray-300
+                    max-w-xl
+                    text-center
+                    md:text-left
+                    ">
           Full Stack Developer skilled in MERN Stack,
           PHP, ReactJS, NodeJS and scalable web applications.
         </p>
 
-        <div className="flex gap-5 mt-8">
+        <div
+            className="
+            flex
+            flex-col
+            sm:flex-row
+            gap-4
+            mt-8
+            justify-center
+            md:justify-start
+          "
+          >
 
   {/* Hire Me Button */}
 
@@ -64,7 +101,15 @@ function Hero() {
 
       </div>
 
-      <div className="w-full md:w-[500px] h-[500px]">
+      <div
+  className="
+  w-full
+  h-[280px]
+  sm:h-[350px]
+  md:h-[500px]
+  md:w-[500px]
+"
+>
 
         <Canvas>
 
@@ -72,14 +117,20 @@ function Hero() {
 
           <directionalLight position={[2,2,2]} />
 
-          <Sphere args={[1,100,200]} scale={2.4}>
+          <Sphere args={[1,64,64]} scale={1.8}>
             <meshStandardMaterial
               color="#915EFF"
-              wireframe
+              metalness={0.7}
+              roughness={0.2}
             />
           </Sphere>
 
-          <OrbitControls enableZoom={false} autoRotate />
+          <OrbitControls
+            enableZoom={false}
+            autoRotate
+            autoRotateSpeed={2}
+            enablePan={false}
+          />
 
         </Canvas>
 
